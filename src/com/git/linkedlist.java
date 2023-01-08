ed4b091d2e58a1447bee303d1f916fd59d222e9c
ashwininangare <nangareashwini1997@gmail.com>
2023-01-08 10:28:20 +0530

    linkedlist int data

---
+++ src/com/git/linkedlist.java
@@ -0,0 +1,16 @@
+package com.git;
+
+import java.util.LinkedList;
+
+public class linkedlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<Integer> llist= new LinkedList<Integer>();
+		llist.add(2);
+		llist.add(4);
+		System.out.println(llist);
+	}
+
+}
