a81ef3a43e18145d455cb1b656c1ad1dfe3afe9e
ashwininangare <nangareashwini1997@gmail.com>
2023-01-08 10:22:35 +0530

    ArrayList data

---
+++ src/com/git/collection/collection.java
@@ -0,0 +1,19 @@
+package com.git.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class collection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	List<String> list = new	ArrayList<String>();
+	list.add("ashu");
+	list.add("neha");
+	list.add("bhumi");
+	System.out.println("list is .."+list);
+	
+	}
+
+}
